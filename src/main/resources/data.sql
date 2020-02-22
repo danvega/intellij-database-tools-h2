2efd7009d0f18f864681b051e829c7d87b8bf56c
Dan Vega <danvega@gmail.com>
2020-02-22 11:09:43 -0500

    initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TBL_EMPLOYEES (first_name, last_name, email) VALUES
+  ('Dan', 'Vega', 'danvega@gmail.com'),
+  ('Jen', 'Vega', 'jenvega@email.com'),
+  ('Bella', 'Vega', 'bellavega@gmail.com');
